db414288c12a3818b08835227c4d013dc2691f91
Richard & 2B <richard.yabrudy.7e6@itb.cat>
2024-10-15 12:55:07 +0200

    FULL BOX 1.0

--- RichardYabrudyIncidencias.py
+++ RichardYabrudyIncidencias.py
@@ -11,7 +11,7 @@ def validar_fecha(fecha_str):
         return False
 
 
-def extraer_fechas_y_validar(xml_file):
+def extraer_incidencias(xml_file):
     # Cargar y parsear el archivo XML
     tree = ET.parse(xml_file)
     root = tree.getroot()
@@ -19,28 +19,37 @@ def extraer_fechas_y_validar(xml_file):
     # Espacio de nombres en XML
     ns = {'ns1': 'http://www.exemple.com/IncidenciasGrup4'}
 
-    # Inicializar contadores
-    total_fechas = 0
-    total_fechas_validas = 0
+    # Inicializar listas
+    lista_todas_incidencias = []  # Lista para almacenar todas las incidencias
+    lista_incidencias_validas = []  # Lista para almacenar las incidencias válidas
 
     # Iterar sobre cada incidencia en el XML
     for incidencia in root.findall('.//ns1:Incidencia', ns):
-        # Extraer la fecha de incidencia
-        fecha_incidencia = incidencia.find('ns1:DataIncidencia', ns)
-        if fecha_incidencia is not None:
-            fecha_incidencia_str = fecha_incidencia.text
-            total_fechas += 1
+        # Extraer todos los elementos de la incidencia
+        incidencia_data = {}
+        for elemento in incidencia:
+            # Guardar los datos de cada elemento en un diccionario
+            incidencia_data[elemento.tag.split('}')[1]] = elemento.text
 
-            # Validar la fecha
-            if validar_fecha(fecha_incidencia_str):
-                total_fechas_validas += 1
+        # Añadir la incidencia completa a la lista de todas las incidencias
+        lista_todas_incidencias.append(incidencia_data)
 
-    print(f'Total de fechas encontradas: {total_fechas}')
-    print(f'Total de fechas válidas: {total_fechas_validas}')
+        # Validar la fecha de la incidencia
+        if 'DataIncidencia' in incidencia_data and validar_fecha(incidencia_data['DataIncidencia']):
+            lista_incidencias_validas.append(incidencia_data)  # Añadir a la lista de válidas
+
+    # Imprimir resultados
+    print(f'Total de incidencias encontradas: {len(lista_todas_incidencias)}')
+    print(f'Total de incidencias válidas: {len(lista_incidencias_validas)}')
+
+    # Imprimir las incidencias válidas
+    print('Incidencias válidas:')
+    for inc in lista_incidencias_validas:
+        print(inc)
 
 
 # Ruta del archivo XML
-ruta_archivo_xml = "/home/richard.yabrudy.7e6/Escriptori/DADES/Richard Yabrudy/Grup 4 - XML Con Excel.xml"
+ruta_archivo_xml = "./Grup 4 - XML Con Excel.xml"
 
 # Llama a la función con la ruta a tu archivo XML
-extraer_fechas_y_validar(ruta_archivo_xml)
+extraer_incidencias(ruta_archivo_xml)
